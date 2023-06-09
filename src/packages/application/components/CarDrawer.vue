18a01da3bc409807ea915c831aa9a710ee958051
susan muraya <susanmuraya89@gmail.com>
2023-06-09 16:59:08 +0300

    all cars

---
+++ src/packages/application/components/CarDrawer.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <v-navigation-drawer
+      v-model="drawer"
+      right
+      fixed
+      height="100%"
+      width="400"
+      temporary
+      v-click-outside="onClickOutSide"
+    >
+      <v-card>
+        <v-spacer></v-spacer>
+        <v-icon color="error">mdi-close</v-icon>
+      </v-card>
+    </v-navigation-drawer>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "CarDrawer",
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+  mounted() {
+    Event.$on("openDrawer", () => {
+      setTimeout(() => {
+        this.drawer = true;
+      }, 200);
+    });
+  },
+  methods: {
+    onClickOutSide() {
+      if (!this.drawer) return;
+      setTimeout(() => {
+        if (this.drawer) this.drawer = false;
+      }, 200);
+    },
+  },
+};
+</script>
